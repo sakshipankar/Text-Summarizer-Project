d68d84f13bab95f718c2308b3b5f5fb46e0c698f
sakshipankar <sakshipankar1124@gmail.com>
2025-03-09 18:38:09 +0530

    commite

---
+++ src/textSummarizer/logging/__init__.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)  # Ensure the directory exists
+
+logging.basicConfig(
+    level=logging.INFO,  # Corrected 'Level' to 'level'
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)  # Fixed 'sys.stout' typo
+    ]
+)
+
+logger = logging.getLogger("textSummarizationLogger")
