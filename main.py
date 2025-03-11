047bac707198f283959bcd5ef49779538a95c8de
sakshipankar <sakshipankar1124@gmail.com>
2025-03-11 23:09:14 +0530

    Fixed file location

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from textSummarizer.logging import logger
 
 logger.info("Starting main.py execution.")
 
-try:
-    logger.info("This log should be written to running_logs.log")
-    print("Logging inside main.py should work.")
-except Exception as e:
-    print(f"Logging failed: {e}")
-    logger.error(f"Logging failed: {e}")
+# try:
+#     logger.info("This log should be written to running_logs.log")
+#     print("Logging inside main.py should work.")
+# except Exception as e:
+#     print(f"Logging failed: {e}")
+#     logger.error(f"Logging failed: {e}")
 
 logger.info("End of main.py execution.")
