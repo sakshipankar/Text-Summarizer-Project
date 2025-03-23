7faa2524f5bc0b0108f388f1a29bdbd97e58a303
sakshipankar <sakshipankar1124@gmail.com>
2025-03-23 16:50:30 +0530

    data transformation done

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,9 @@
 from src.textSummarizer.utils.common import read_yaml, create_directories
 from src.textSummarizer.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from src.textSummarizer.entity.config_entity import DataIngestionConfig
+from src.textSummarizer.entity.config_entity import DataValidationConfig
+from src.textSummarizer.entity.config_entity import DataTransformationConfig
+
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
@@ -22,3 +25,29 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+            config = self.config.data_transformation
+
+            create_directories([config.root_dir])
+
+            data_transformation_config = DataTransformationConfig(
+                root_dir=config.root_dir,
+                data_path=config.data_path,
+                tokenizer_name = config.tokenizer_name
+            )
+
+            return data_transformation_config
